de7d67e944d896444a8c4b8baffc991454e3ac1d
tecnolochia <arce02377@gmail.com>
2025-08-11 20:12:21 +0000

    11.09.2025

---
+++ 11deagosto.js
@@ -0,0 +1,19 @@
+let nombreDeVariable = "el texto siempre va entre comillas"
+nombreDeVariable = nombreDeVariable + ", tambien podemos sumar textos"
+console.log(nombreDeVariable)
+nombreDeVariable = 2
+
+let suma = 50 + 1
+nombreDeVariable += 2
+nombreDeVariable++
+let multiplicar = 1300 * 50
+let dividir = 100 / 8
+console.log(dividir)
+
+let verdadero = true
+let falso = false
+let booleano = dividir == 12.5
+let mayorIgual = 1 >= 0
+let menorIgual = 1 <= 0
+let los2sonVerdaderos = true && false
+console.log(booleano)
